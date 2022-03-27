2d6666fb653f1572147cf97c9680a40db5fbb23f
Ricardo Costa <ricardo18acs@gmail.com>
2022-03-27 09:57:02 -0300

    feat: format file

--- hello-world/app.js
+++ hello-world/app.js
@@ -7,46 +7,44 @@ let response;
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ *
  */
 exports.lambdaHandler = async (event, context) => {
-    try {
-
-      let specialBody = {}
-      switch (event?.httpMethod) {
-        case "POST":
-          specialBody = JSON.stringify({
-            message: event.body.message
-          })
-        default:
-          specialBody = JSON.stringify({
-            message: 'hello world'
-          })
-
-      }
-
-        // const ret = await axios(url);
-        // response = {
-        //     'statusCode': 200,
-        //     'body': JSON.stringify({
-        //         message: 'hello world',
-        //         // message: event.body.message,
-        //         // location: ret.data.trim()
-        //     })
-        // }
-        response = {
-            'statusCode': 200,
-            'body': specialBody
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
+  try {
+    let specialBody = {};
+    switch (event?.httpMethod) {
+      case "POST":
+        specialBody = JSON.stringify({
+          message: event.body.message,
+        });
+      default:
+        specialBody = JSON.stringify({
+          message: "hello world",
+        });
     }
 
-    return response
+    // const ret = await axios(url);
+    // response = {
+    //     'statusCode': 200,
+    //     'body': JSON.stringify({
+    //         message: 'hello world',
+    //         // message: event.body.message,
+    //         // location: ret.data.trim()
+    //     })
+    // }
+    response = {
+      statusCode: 200,
+      body: specialBody,
+    };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+
+  return response;
 };
